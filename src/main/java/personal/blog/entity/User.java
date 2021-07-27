4b23d121377af8401c59e497ea872ac757228cfa
phuc <nguyenphuc.vna@gmail.com>
2021-07-27 10:46:15 +0700

    fix user table name

--- src/main/java/personal/blog/entity/User.java
+++ src/main/java/personal/blog/entity/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_tb")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
